a53f4de2b90edf4d6e59babe260178e9bc7c8e3d
Matheus-Noleto <matheusnsampaio@gmail.com>
2022-04-30 10:31:41 -0300

    Criação das cenas e controladores tela de login, cadastro e tela principal

---
+++ src/main/java/br/ufrpe/habitact/gui/TelaDeCadastroControlador.java
@@ -0,0 +1,16 @@
+package br.ufrpe.habitact.gui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class TelaDeCadastroControlador extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
---
+++ src/main/java/br/ufrpe/habitact/gui/TelaDeLoginControlador.java
@@ -0,0 +1,41 @@
+package br.ufrpe.habitact.gui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.TabPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class TelaDeLoginControlador extends Application {
+    private TabPane principal;
+    private Stage primaryStage;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("Login");
+
+        initMainStage();
+    }
+
+    private void initMainStage() throws IOException {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(TelaDeLoginControlador.class.getResource("gui/TelaDeLogin.fxml"));
+            this.principal = (TabPane) loader.load();
+
+            Scene cena = new Scene(this.principal);
+            this.primaryStage.setScene(cena);
+            this.primaryStage.show();
+        }catch (IOException ioe){
+            throw new IOException("IOException");
+        }
+
+    }
+}

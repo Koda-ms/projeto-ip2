befbd2c8c4e618a2fec678eb78ca5875a9f6ef57
Matheus-Noleto <matheusnsampaio@gmail.com>
2022-05-03 11:05:35 -0300

    algumas mudancas nas telas

--- src/main/java/br/ufrpe/habitact/gui/TelaDeLoginControlador.java
+++ src/main/java/br/ufrpe/habitact/gui/TelaDeLoginControlador.java
@@ -1,41 +1,40 @@
 package br.ufrpe.habitact.gui;
 
+import br.ufrpe.habitact.dados.IRepositorio;
+import br.ufrpe.habitact.negocio.beans.Usuario;
 import javafx.application.Application;
+import javafx.beans.property.StringProperty;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.TabPane;
+import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
-public class TelaDeLoginControlador extends Application {
-    private TabPane principal;
-    private Stage primaryStage;
+import static javafx.application.Application.launch;
+
+public class TelaDeLoginControlador{
+    @FXML private TitledPane principal;
+    @FXML private AnchorPane telaDeCadastro;
+    @FXML private AnchorPane subTelaDeCadastro;
+    @FXML private PasswordField email;
+    @FXML private PasswordField senha;
+    @FXML private Text txtEmail;
+    @FXML private Text txtSenha;
+    @FXML private Button criarConta;
+    @FXML private Button logar;
+    private ArrayList<Usuario> listaUsuarios;
+    private IRepositorio<Usuario> repositorioUsuarios;
+
 
     public static void main(String[] args) {
         launch(args);
     }
 
-    @Override
-    public void start(Stage primaryStage) throws IOException{
-        this.primaryStage = primaryStage;
-        this.primaryStage.setTitle("Login");
-
-        initMainStage();
-    }
-
-    private void initMainStage() throws IOException {
-        try {
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(TelaDeLoginControlador.class.getResource("gui/TelaDeLogin.fxml"));
-            this.principal = (TabPane) loader.load();
-
-            Scene cena = new Scene(this.principal);
-            this.primaryStage.setScene(cena);
-            this.primaryStage.show();
-        }catch (IOException ioe){
-            throw new IOException("IOException");
-        }
-
-    }
 }

f543492b3ef79a8369cb18b8a38319e266b10f1f
Matheus-Noleto <matheusnsampaio@gmail.com>
2022-05-06 18:16:45 -0300

    Adicionando o enum dias da semana

---
+++ src/main/java/br/ufrpe/habitact/negocio/beans/enums/DiasDaSemana.java
@@ -0,0 +1,22 @@
+package br.ufrpe.habitact.negocio.beans.enums;
+
+public class DiasDaSemana {
+    public enum Refeicao {
+        SEGUNDA_FEIRA ("Segunda-feira"),
+        TERCA_FEIRA ("Terça-feira"),
+        QUARTA_FEIRA ("Quarta-feira"),
+        QUINTA_FEIRA ("Quinta-feira"),
+        SEXTA_FEIRA ("Sexta-feira"),
+        SABADO ("Sábado"),
+        DOMINGO ("Domingo");
+
+        private String diaDaSemana;
+
+        Refeicao(String diaDaSemana){
+            this.diaDaSemana = diaDaSemana;
+        }
+
+        public String getRefeicao() {
+            return diaDaSemana;
+        }
+}

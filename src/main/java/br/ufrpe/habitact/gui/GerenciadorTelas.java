8cb6e484471ccb9aaacea04263857dc3426ba15d
Julyanne Correia <julyanne.es@gmail.com>
2022-05-06 22:19:06 -0300

    Add de novas telas e método alertaErro

--- src/main/java/br/ufrpe/habitact/gui/GerenciadorTelas.java
+++ src/main/java/br/ufrpe/habitact/gui/GerenciadorTelas.java
@@ -3,6 +3,7 @@ package br.ufrpe.habitact.gui;
 import br.ufrpe.habitact.HelloApplication;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,12 +12,18 @@ public class GerenciadorTelas {
 
     private static GerenciadorTelas instance;
     private Stage primaryStage;
-    private Scene planoAlimentarScene;
-    private TelaCadastroPlanoAlimentarController cadastroPlanoAlimentarController;
     private Scene popupScene;
     private PopupCadastroPlanosController cadastroPlanosController;
+    private Scene planoAlimentarScene;
+    private TelaCadastroPlanoAlimentarController cadastroPlanoAlimentarController;
     private Scene addAlimentoScene;
     private TelaCadastrarAlimentosController cadastrarAlimentosController;
+    private Scene planoTreinoScene;
+    private TelaCadastroPlanoTreinoController cadastroPlanoTreinoController;
+    private Scene addTreino;
+    private TelaCadastrarTreinoController cadastrarTreinoController;
+    private Scene addExercicioScene;
+    private TelaCadastrarExercicioController cadastrarExercicioController;
     private GerenciadorTelas(){
         this.initialize();
     }
@@ -30,7 +37,6 @@ public class GerenciadorTelas {
 
     private void initialize() {
         FXMLLoader fxmlLoader = new FXMLLoader();
-        //Parent mainPane = null;
         try {
             fxmlLoader = new FXMLLoader(HelloApplication.class.
                     getResource("PopupCadastroPlanos.fxml"));
@@ -48,26 +54,67 @@ public class GerenciadorTelas {
                     getResource("TelaCadastrarAlimentos.fxml"));
             this.addAlimentoScene = new Scene(fxmlLoader.load());
             this.cadastrarAlimentosController = (TelaCadastrarAlimentosController) fxmlLoader.getController();
+
+            fxmlLoader = new FXMLLoader();
+            fxmlLoader = new FXMLLoader(HelloApplication.class.
+                    getResource("TelaCadastroPlanoTreino.fxml"));
+            this.planoTreinoScene = new Scene(fxmlLoader.load());
+            this.cadastroPlanoTreinoController = (TelaCadastroPlanoTreinoController) fxmlLoader.getController();
+
+            fxmlLoader = new FXMLLoader();
+            fxmlLoader = new FXMLLoader(HelloApplication.class.
+                    getResource("TelaCadastrarTreino.fxml"));
+            this.addTreino = new Scene(fxmlLoader.load());
+            this.cadastrarTreinoController = (TelaCadastrarTreinoController) fxmlLoader.getController();
+
+            fxmlLoader = new FXMLLoader();
+            fxmlLoader = new FXMLLoader(HelloApplication.class.
+                    getResource("TelaCadastrarExercicio.fxml"));
+            this.addExercicioScene = new Scene(fxmlLoader.load());
+            this.cadastrarExercicioController = (TelaCadastrarExercicioController) fxmlLoader.getController();
+
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-
+    //TODO Completar método com todas as telas
     public void trocarTela(String tela){
         FXMLLoader fxmlLoader = new FXMLLoader();
-        if (tela.equalsIgnoreCase("planoAlimentar")){
-            primaryStage.setScene(planoAlimentarScene);
+
+        switch (tela){
+            case "planoAlimentar": primaryStage.setScene(planoAlimentarScene);break;
+            case "planoTreino": primaryStage.setScene(planoTreinoScene); break;
         }
     }
 
-    public Scene getAddAlimentoScene() { return addAlimentoScene; }
+    public void alertaCamposVazios(){
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Erro no Cadastro");
+        alert.setHeaderText("Campo vazio");
+        alert.setContentText("Verifique o(s) campo(s) vazio(s) do seu cadastro");
+        alert.showAndWait();
+    }
+
+    public Scene getPopupScene() {
+        return popupScene;
+    }
 
     public Scene getPlanoAlimentarScene() {
         return planoAlimentarScene;
     }
 
-    public Scene getPopupScene() {
-        return popupScene;
+    public Scene getAddAlimentoScene() { return addAlimentoScene; }
+
+    public Scene getPlanoTreinoScene() {
+        return planoTreinoScene;
+    }
+
+    public Scene getAddTreino() {
+        return addTreino;
+    }
+
+    public Scene getAddExercicioScene() {
+        return addExercicioScene;
     }
 
     public Stage getPrimaryStage() {
@@ -78,15 +125,27 @@ public class GerenciadorTelas {
         this.primaryStage = primaryStage;
     }
 
-    public TelaCadastroPlanoAlimentarController getCadastroPlanoAlimentarController() {
-        return cadastroPlanoAlimentarController;
-    }
-
     public PopupCadastroPlanosController getCadastroPlanosController() {
         return cadastroPlanosController;
     }
 
+    public TelaCadastroPlanoAlimentarController getCadastroPlanoAlimentarController() {
+        return cadastroPlanoAlimentarController;
+    }
+
     public TelaCadastrarAlimentosController getCadastrarAlimentosController() {
         return cadastrarAlimentosController;
+    }
+
+    public TelaCadastroPlanoTreinoController getCadastroPlanoTreinoController() {
+        return cadastroPlanoTreinoController;
+    }
+
+    public TelaCadastrarTreinoController getCadastrarTreinoController() {
+        return cadastrarTreinoController;
+    }
+
+    public TelaCadastrarExercicioController getCadastrarExercicioController() {
+        return cadastrarExercicioController;
     }
 }

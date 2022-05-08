aabcfd10799b63d614f6d473cd9bd538e3e5b250
Julyanne Correia <julyanne.es@gmail.com>
2022-05-08 02:38:09 -0300

    Updates para inicialização e atulaização das TableViews

--- src/main/java/br/ufrpe/habitact/gui/TelaCadastrarTreinoController.java
+++ src/main/java/br/ufrpe/habitact/gui/TelaCadastrarTreinoController.java
@@ -1,31 +1,50 @@
 package br.ufrpe.habitact.gui;
 
 import br.ufrpe.habitact.excecoes.ObjetoDuplicadoException;
+import br.ufrpe.habitact.gui.modelos.ModeloTreino;
 import br.ufrpe.habitact.negocio.Fachada;
 import br.ufrpe.habitact.negocio.beans.Exercicio;
 import br.ufrpe.habitact.negocio.beans.Treino;
 import br.ufrpe.habitact.negocio.beans.enums.CategoriaTreino;
+import br.ufrpe.habitact.negocio.beans.enums.RitmoDoExercicio;
+import br.ufrpe.habitact.negocio.beans.enums.TipoExercicio;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
-import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.CheckBoxTableCell;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class TelaCadastrarTreinoController {
 
     @FXML private Button btnCancelarSalvarPressed;
-    @FXML private TableView<Exercicio> tblExercicio;
-    @FXML private TableColumn<?, ?> nomeExercicio;
-    @FXML private TableColumn<?, ?> ritmo;
-    @FXML private TableColumn<?, ?> calQueimadas;
-    @FXML private TextField textDuracao;
     @FXML private ComboBox<CategoriaTreino> optCategoria;
 
+    @FXML private TableView<ModeloTreino> tblExercicio;
+    @FXML private TableColumn<ModeloTreino, Boolean> colunaCheck;
+    @FXML private TableColumn<ModeloTreino, String> colunaNomeExercicio;
+    @FXML private TableColumn<ModeloTreino, String> colunaRitmo;
+    @FXML private TableColumn<ModeloTreino, Double> colunaCalorias; //Atributo que aparecerá na TableView da tela CadastrarPlanoTreio
+
     public void initialize(){
-        optCategoria.getItems().addAll(CategoriaTreino.values());
+        //Add valores ao ComboBox
+        this.optCategoria.getItems().addAll(CategoriaTreino.values());
+        //Setando as colunas da TableView
+        this.colunaCheck.setCellValueFactory(new PropertyValueFactory<>("check"));
+        this.colunaCheck.setCellFactory(CheckBoxTableCell.forTableColumn(colunaCheck));
+        this.colunaNomeExercicio.setCellValueFactory(new PropertyValueFactory<>("nome"));
+        this.colunaRitmo.setCellValueFactory(new PropertyValueFactory<>("ritmo"));
+        this.updateCatalogoExercicios();
     }
 
     @FXML
@@ -45,13 +64,12 @@ public class TelaCadastrarTreinoController {
             GerenciadorTelas.getInstance().alertaCamposVazios();
         } else{
             //TODO Configurar parte para armazenar os treinos cadastrados pelo usuário
-            Treino t = new Treino((ArrayList<Exercicio>) tblExercicio.getItems(), optCategoria.getValue());
+            Treino t = new Treino((ArrayList<Exercicio>) Fachada.getInstance().listarExercicios(), optCategoria.getValue());
 
             try {
                 Fachada.getInstance().inserirTreino(t);
-                //Fachada.getInstance().inserirTreinoNoPlano();
             } catch (ObjetoDuplicadoException e) {
-                throw new RuntimeException(e);
+                e.getMessage();
             }
             this.limparCamposDeDados();
             ((Stage)this.btnCancelarSalvarPressed.getScene().getWindow()).close();
@@ -59,8 +77,30 @@ public class TelaCadastrarTreinoController {
 
     }
 
+    private void updateCatalogoExercicios(){
+        //Instancia exercícios para o Controlador de Exercício
+        ArrayList<Exercicio> listaExercicios = new ArrayList<>();
+        Exercicio ex1 = new Exercicio(TipoExercicio.CAMINHADA, RitmoDoExercicio.BAIXO, 10, 3, 2);
+        Exercicio ex2 = new Exercicio(TipoExercicio.CORRIDA, RitmoDoExercicio.MEDIO, 60, 1, 1);
+        Exercicio ex3 = new Exercicio(TipoExercicio.JIU_JITSU, RitmoDoExercicio.ALTO, 15, 2, 3);
+        //Add o exercícios ao ControladorExercicio
+        try {
+            Fachada.getInstance().inserirExercicios(ex1);
+            Fachada.getInstance().inserirExercicios(ex2);
+            Fachada.getInstance().inserirExercicios(ex3);
+        } catch (ObjetoDuplicadoException e) {
+            e.printStackTrace();
+        }
+        //Lista os exercícios adicionados na tabela Catálogo de Exercícios
+        ObservableList<ModeloTreino> result = FXCollections.observableArrayList();
+        List<Exercicio> listaExerc = Fachada.getInstance().listarExercicios();
+        for (Exercicio ex : listaExerc){
+            result.add(new ModeloTreino(ex));
+        }
+        tblExercicio.setItems(result);
+    }
+
     private void limparCamposDeDados() {
-        this.textDuracao.setText("");
         this.optCategoria.getSelectionModel().clearSelection();
         this.tblExercicio.getSelectionModel().clearSelection();
     }
--- src/main/java/br/ufrpe/habitact/gui/TelaCadastroPlanoTreinoController.java
+++ src/main/java/br/ufrpe/habitact/gui/TelaCadastroPlanoTreinoController.java
@@ -1,29 +1,52 @@
 package br.ufrpe.habitact.gui;
 
-import br.ufrpe.habitact.negocio.beans.Exercicio;
+import br.ufrpe.habitact.excecoes.ObjetoDuplicadoException;
+import br.ufrpe.habitact.gui.modelos.ModeloPlanoTreinoCliente;
+import br.ufrpe.habitact.negocio.Fachada;
 import br.ufrpe.habitact.negocio.beans.Treino;
+import br.ufrpe.habitact.negocio.beans.enums.CategoriaTreino;
 import br.ufrpe.habitact.negocio.beans.enums.ObjetivoTreino;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.CheckBoxTableCell;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public class TelaCadastroPlanoTreinoController {
 
-    @FXML private Button btnCancelarCadastrarPressed;
     @FXML private DatePicker dtFim;
     @FXML private DatePicker dtInicio;
-    @FXML private TableView<Treino> tblTreino;
-    @FXML private TableColumn<Treino, String> nomeTreino;
-    @FXML private TableColumn<Exercicio, String> detalheExercicio; //Apenas o nome do exercício será mostrado
-    @FXML private ComboBox<ObjetivoTreino> objetivoTreino;
     @FXML private TextField textOutro;
+    @FXML private Button btnCancelarCadastrarPressed;
+    @FXML private ComboBox<ObjetivoTreino> objetivoTreino;
+
+    @FXML private TableView<ModeloPlanoTreinoCliente> tblTreino;
+    @FXML private TableColumn<ModeloPlanoTreinoCliente, Boolean> colunaCheck;
+    @FXML private TableColumn<ModeloPlanoTreinoCliente, Double> colunaCalorias;
+    @FXML private TableColumn<ModeloPlanoTreinoCliente, Double> colunaDuracao;
+    @FXML private TableColumn<ModeloPlanoTreinoCliente, String> coluanDiaTreino;
+    @FXML private TableColumn<ModeloPlanoTreinoCliente, String> colunaCategoriaTreino;
 
     public void initialize() {
         //Adiciona os valores do enum ObjetivoTreino no ComboBox
         this.objetivoTreino.getItems().addAll(ObjetivoTreino.values());
+        //Setando as colunas da TableView
+        this.colunaCheck.setCellValueFactory(new PropertyValueFactory<>("check"));
+        this.colunaCheck.setCellFactory(CheckBoxTableCell.forTableColumn(colunaCheck));
+        this.coluanDiaTreino.setCellValueFactory(new PropertyValueFactory<>("data"));
+        this.colunaDuracao.setCellValueFactory(new PropertyValueFactory<>("duracao"));
+        this.colunaCalorias.setCellValueFactory(new PropertyValueFactory<>("calorias"));
+        this.colunaCategoriaTreino.setCellValueFactory(new PropertyValueFactory<>("categoria"));
+        this.updateCatalogoTreino();
     }
 
     @FXML
@@ -71,6 +94,44 @@ public class TelaCadastroPlanoTreinoController {
     @FXML
     void btnVoltarPressed(ActionEvent event) {
         //GerenciadorTelas.getInstance().trocarTela("listarDadosSistema");
+    }
+
+    private void updateCatalogoTreino() {
+        //Instancia treinos para o Controlador de Treinos
+        ArrayList<Treino> listaTreinos = new ArrayList<>();
+        //TODO não conseguindo setar um ArrayList<Exercicio> manualmente
+        Treino t1 = new Treino();
+        t1.setDiaFeito(LocalDate.of(2022, 5, 1));
+        t1.setModalidade(CategoriaTreino.ANAEROBICO);
+        t1.setQueimaCaloricaTotal(80);
+        t1.setDuracao(30);
+        Treino t2 = new Treino();
+        t2.setDiaFeito(LocalDate.of(2022, 5, 7));
+        t2.setModalidade(CategoriaTreino.AEROBICO);
+        t2.setQueimaCaloricaTotal(170);
+        t2.setDuracao(25);
+        Treino t3 = new Treino();
+        t3.setDiaFeito(LocalDate.of(2022, 4, 20));
+        t3.setModalidade(CategoriaTreino.AEROBICO);
+        t3.setQueimaCaloricaTotal(200);
+        t3.setDuracao(50);
+
+        try {
+            Fachada.getInstance().inserirTreino(t1);
+            Fachada.getInstance().inserirTreino(t2);
+            Fachada.getInstance().inserirTreino(t3);
+        } catch (ObjetoDuplicadoException e) {
+           e.printStackTrace();
+        }
+        /*Treino t1 = new Treino((ArrayList<Exercicio>) Fachada.getInstance().listarExercicios(),
+                    CategoriaTreino.AEROBICO);*/
+
+        ObservableList<ModeloPlanoTreinoCliente> result = FXCollections.observableArrayList();
+        List<Treino> listaDeTreinos = Fachada.getInstance().listarTreino();
+        for (Treino t : listaDeTreinos){
+            result.add(new ModeloPlanoTreinoCliente(t));
+        }
+        tblTreino.setItems(result);
     }
 
     private void limparCamposDeDados() {

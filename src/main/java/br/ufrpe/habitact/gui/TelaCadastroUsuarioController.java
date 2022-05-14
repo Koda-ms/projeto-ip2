420050f1ec4e2b13af8607c0d24e3a50c538ae0c
Julyanne Correia <julyanne.es@gmail.com>
2022-05-14 16:21:43 -0300

    Refactor: nome das telas

---
+++ src/main/java/br/ufrpe/habitact/gui/TelaCadastroUsuarioController.java
@@ -0,0 +1,122 @@
+package br.ufrpe.habitact.gui;
+
+import br.ufrpe.habitact.excecoes.ObjetoDuplicadoException;
+import br.ufrpe.habitact.negocio.Fachada;
+import br.ufrpe.habitact.negocio.beans.Administrador;
+import br.ufrpe.habitact.negocio.beans.Cliente;
+import br.ufrpe.habitact.negocio.beans.Usuario;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
+
+public class TelaCadastroUsuarioController {
+
+    @FXML private TextField peso;
+    @FXML private TextField altura;
+    @FXML private TextField genero;
+    @FXML private TextField emailCliente;
+    @FXML private TextField emailAdministrador;
+    @FXML private TextField nomeAdministrador;
+    @FXML private TextField nomeCliente;
+    @FXML private PasswordField senhaCliente;
+    @FXML private PasswordField senhaAdministrador;
+    @FXML private PasswordField confirmacaoSenhaCliente;
+    @FXML private PasswordField confirmacaoDeSenhaAdministrador;
+    @FXML private DatePicker dataNascimentoCliente;
+    @FXML private DatePicker dataNascimentoAdministrador;
+    @FXML private AnchorPane tabAdministrador;
+    @FXML private AnchorPane tabUsuario;
+    private static String Id = "0";
+
+
+    @FXML
+    private void CadastrarUsuarioBtn(ActionEvent e){
+        if(verificarCamposVaziosUsuario()){
+            GerenciadorTelas.getInstance().alertaCamposVazios();
+        }
+        else{
+            try{
+                Fachada.getInstance().cadastrarUsuario(new Cliente(nomeCliente.getText(), emailCliente.getText(),
+                        senhaCliente.getText(), dataNascimentoCliente.getValue(), genero.getText(),
+                        Double.parseDouble(peso.getText()), Double.parseDouble(altura.getText()),
+                        false));
+                gerarAlertaDeCadastro();
+                for (Usuario u : Fachada.getInstance().listarUsuarios()){
+                    System.out.println(u.getNome());
+                }
+            } catch (ObjetoDuplicadoException exception) {
+                exception.printStackTrace();
+            }
+            this.limparCamposDeDados();
+        }
+    }
+
+    @FXML
+    private void CadastrarAdministradorBtn(ActionEvent e){
+        if(verificarCamposVaziosAdministrador()){
+            GerenciadorTelas.getInstance().alertaCamposVazios();
+        }
+        else{
+            try {
+                Fachada.getInstance().cadastrarUsuario(new Administrador(nomeAdministrador.getText(),
+                        emailAdministrador.getText(), senhaAdministrador.getText(),
+                        dataNascimentoAdministrador.getValue(), Id));
+                int idSaver = Integer.parseInt(Id);
+                idSaver++;
+                Id = Integer.toString(idSaver);
+                gerarAlertaDeCadastro();
+                for (Usuario u : Fachada.getInstance().listarUsuarios()){
+                    System.out.println(u.getNome());
+            }
+            } catch (ObjetoDuplicadoException exception) {
+                exception.printStackTrace();
+            }
+            this.limparCamposDeDados();
+        }
+    }
+
+    @FXML
+    void btnVoltarCliente(ActionEvent event) {
+        GerenciadorTelas.getInstance().trocarTela("TelaLogin");
+    }
+
+    @FXML
+    void btnVoltarAdm(ActionEvent event) {
+        GerenciadorTelas.getInstance().trocarTela("TelaLogin");
+    }
+
+    private void gerarAlertaDeCadastro(){
+        Alert alerta = new Alert(Alert.AlertType.CONFIRMATION);
+        alerta.setTitle("Usuário cadastrado");
+        alerta.setHeaderText("O usuário foi cadastrado");
+        alerta.showAndWait();
+    }
+
+    private void limparCamposDeDados() {
+        this.genero.setText("");
+        this.emailCliente.setText("");
+        this.emailAdministrador.setText("");
+        this.senhaCliente.setText("");
+        this.senhaAdministrador.setText("");
+        this.confirmacaoSenhaCliente.setText("");
+        this.confirmacaoDeSenhaAdministrador.setText("");
+        this.nomeCliente.setText("");
+        this.nomeAdministrador.setText("");
+        this.altura.setText("");
+        this.peso.setText("");
+        this.dataNascimentoCliente.setValue(null);
+        this.dataNascimentoAdministrador.setValue(null);
+    }
+
+    private boolean verificarCamposVaziosUsuario() {
+        return genero.getText().isBlank() || nomeCliente.getText().isBlank() ||
+                dataNascimentoCliente.getValue() == null || peso.getText().isBlank() || altura.getText().isBlank() ||
+                emailCliente.getText().isBlank() || senhaCliente.getText().isBlank() || confirmacaoSenhaCliente.getText().isBlank();
+    }
+
+    private boolean verificarCamposVaziosAdministrador() {
+        return nomeAdministrador.getText().isBlank() || emailAdministrador.getText().isBlank() ||
+                senhaAdministrador.getText().isBlank() || confirmacaoDeSenhaAdministrador.getText().isBlank() || dataNascimentoAdministrador.getValue() == null;
+    }
+}
---
+++ src/main/java/br/ufrpe/habitact/gui/TelaInfoPessoalClienteController.java
@@ -0,0 +1,82 @@
+package br.ufrpe.habitact.gui;
+
+import br.ufrpe.habitact.excecoes.ObjetoNaoExisteException;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+public class TelaInfoPessoalClienteController {
+    @FXML TextField nome;
+    @FXML TextField email;
+    @FXML PasswordField senha;
+    @FXML DatePicker nascimento;
+    @FXML PasswordField btnActualPassword;
+    @FXML PasswordField newPassword;
+    @FXML Button editar;
+    @FXML Button salvarDados;
+    @FXML Button voltar;
+    @FXML Button salvarSenha;
+
+    private void initialize() {
+//        Usuario user /* pegar instância de usuario logada no momento */;
+//
+//        nome.setText(user.getNome());
+//        email.setText(user.getEmail());
+//        nascimento.getEditor().setText(user.getDtNascimento());
+    }
+
+//    @FXML
+//    public void btnEditarDadosPressed() {
+//        this.ativarEdicaoDados(true);
+//    }
+
+    @FXML
+    public void btnSalvarDadosPressed() throws ObjetoNaoExisteException{
+//            Usuario user =  /*pegar instância de usuario logada no momento*/;
+//            user.setNome(nome.getText());
+//            user.setEmail(email.getText());
+//            user.setDtNascimento(nascimento.getText());
+//            if(!user){
+//                throw new ObjetoNaoExisteException("Não foi possivel alterar os dados do usuario");
+//            }
+//            else {
+//                this.ativarEdicaoDados(false);
+//                this.senha.setText("");
+//            }
+    }
+
+    @FXML
+    public void btnSalvarSenhaPressed() {
+        if (senha.getText().equals(newPassword.getText())) {
+            //Usuario user = /*pegar instância de usuario logada no momento*/;
+            this.newPassword.setText("");
+        } else {
+            this.gerarAlertaErroAutenticacao("As senhas não batem!");
+        }
+        this.btnActualPassword.setText("");
+    }
+
+    @FXML
+    public void btnVoltarTelaPressed() {
+       GerenciadorTelas.getInstance().trocarTela("TelaPrincipalCliente");
+    }
+
+    private void ativarEdicaoDados(boolean edicao) {
+        nome.setEditable(edicao);
+        nome.setDisable(!edicao);
+        senha.setEditable(edicao);
+        senha.setDisable(!edicao);
+        email.setEditable(edicao);
+        email.setDisable(!edicao);
+        nascimento.setEditable(edicao);
+        nascimento.setDisable(!edicao);
+    }
+
+    private void gerarAlertaErroAutenticacao(String justificativa) {
+        Alert alerta = new Alert(Alert.AlertType.ERROR);
+        alerta.setTitle("Erro de Autenticação");
+        alerta.setHeaderText("Parece que tivemos um erro com sua tentativa de alteração de dados");
+        alerta.setContentText(justificativa);
+        alerta.showAndWait();
+    }
+
+}

f7905d8f1df6994082bb1a319b441d780382639f
Julyanne Correia <julyanne.es@gmail.com>
2022-05-21 00:51:24 -0300

    Update Sessao.java

--- src/main/java/br/ufrpe/habitact/sessao/Sessao.java
+++ src/main/java/br/ufrpe/habitact/sessao/Sessao.java
@@ -1,12 +1,14 @@
 package br.ufrpe.habitact.sessao;
 
-import br.ufrpe.habitact.negocio.beans.PlanoAlimentar;
-import br.ufrpe.habitact.negocio.beans.Usuario;
+import br.ufrpe.habitact.negocio.beans.*;
 
 public class Sessao {
     private static Sessao instance;
     private Usuario usuario;
     private PlanoAlimentar planoAlimentar;
+    private PlanoTreino planoTreino;
+    private Treino treino;
+    private Exercicio exercicio;
 
     public static Sessao getInstance() {
         if (instance == null) {
@@ -29,5 +31,29 @@ public class Sessao {
 
     public void setPlanoAlimentar(PlanoAlimentar planoAlimentar) {
         this.planoAlimentar = planoAlimentar;
+    }
+
+    public PlanoTreino getPlanoTreino() {
+        return planoTreino;
+    }
+
+    public void setPlanoTreino(PlanoTreino planoTreino) {
+        this.planoTreino = planoTreino;
+    }
+
+    public Treino getTreino() {
+        return treino;
+    }
+
+    public void setTreino(Treino treino) {
+        this.treino = treino;
+    }
+
+    public Exercicio getExercicio() {
+        return exercicio;
+    }
+
+    public void setExercicio(Exercicio exercicio) {
+        this.exercicio = exercicio;
     }
 }
